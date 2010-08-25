399196f615de4edef66672eaa15bde3ea3150248
Pratul Kalia <pratul@pratul.in>
2010-08-25 16:02:26 +0530

    First commit!

---
+++ src/net/agropedia/pop/POPHome.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2010 (c) Pratul Kalia
+ */
+
+package net.agropedia.pop;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class POPHome extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
+
