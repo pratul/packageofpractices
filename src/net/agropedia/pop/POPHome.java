7c23fc3a7e5113eda184390f6dc3316a6112977c
Pratul Kalia <pratul@pratul.in>
2010-08-31 16:36:39 +0530

    - Fixed gitignore, removed incorrectly added files - Options menu added - Start of the list activity + data adapter

--- src/net/agropedia/pop/POPHome.java
+++ src/net/agropedia/pop/POPHome.java
@@ -4,15 +4,81 @@
 
 package net.agropedia.pop;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.ArrayList;
 
-public class POPHome extends Activity {
-    /** Called when the activity is first created. */
+import android.app.ListActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Toast;
+
+public class POPHome extends ListActivity {
+
+	private HomeAdapter mLVAdapter;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        setListAdapter(mLVAdapter);
+
     }
+
+
+    @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inf = getMenuInflater();
+		inf.inflate(R.menu.about, menu);
+		return true;
+
+	}
+
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.mnu_about:
+			Context context = getApplicationContext();
+			CharSequence text = "Agroid\nhttp://agropedia.iitk.ac.in";
+			int duration = Toast.LENGTH_LONG;
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.setGravity(Gravity.CENTER, 0, 0);
+			toast.show();
+
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+
+	public class HomeAdapter extends BaseAdapter {
+
+    	private final ArrayList<String> mItemsList = new ArrayList<String>();
+
+		public int getCount() {
+			return mItemsList.size();
+		}
+
+		public Object getItem(int position) {
+			return mItemsList.get(position);
+		}
+
+		public long getItemId(int position) {
+			return position;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			return convertView;
+		}
+
+    }
+
 }
 
