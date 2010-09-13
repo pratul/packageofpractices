643eb47c29eb030ae78a66f1c0a48c38be430ae2
Pratul Kalia <pratul@pratul.in>
2010-09-13 13:03:58 +0530

    Fixed font location. Added browser intent. Increased font size.

--- src/net/agropedia/pop/POPHome.java
+++ src/net/agropedia/pop/POPHome.java
@@ -5,17 +5,22 @@
 package net.agropedia.pop;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class POPHome extends ListActivity {
@@ -26,13 +31,37 @@ public class POPHome extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mLVAdapter = new HomeAdapter();
         setListAdapter(mLVAdapter);
 
     }
 
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+
+		switch (position) {
+		case 0:
+			Intent popList = new Intent(getBaseContext(), POPlist.class);
+			startActivity(popList);
+		case 1:
+//			Intent browser = new Intent(Intent.ACTION_VIEW, Uri.parse("http://agropedia.iitk.ac.in"));
+//			startActivity(browser);
+
+		}
+
+	}
+
+
 	public class HomeAdapter extends BaseAdapter {
 
-    	private final ArrayList<String> mItemsList = new ArrayList<String>();
+    	private final ArrayList<String> mItemsList;
+    	private final LayoutInflater mInflater;
+
+    	public HomeAdapter() {
+    		mItemsList = new ArrayList<String>(Arrays.asList("Package of Practices", "Agropedia.net on the web"));
+    		mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    	}
 
 		public int getCount() {
 			return mItemsList.size();
@@ -47,6 +76,14 @@ public class POPHome extends ListActivity {
 		}
 
 		public View getView(int position, View convertView, ViewGroup parent) {
+	    	final TextView tv;
+
+			if (convertView == null) {
+				convertView = mInflater.inflate(R.layout.lv_home_row, null);
+			}
+
+			tv = (TextView) convertView.findViewById(R.id.tv_homescreen_row);
+			tv.setText(mItemsList.get(position));
 			return convertView;
 		}
 
---
+++ src/net/agropedia/pop/POPlist.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2010 (c) Pratul Kalia
+ */
+
+package net.agropedia.pop;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class POPlist extends ListActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+	}
+
+}
