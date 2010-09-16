1529d050f15bda955be010013436265c02b76e23
Pratul Kalia <pratul@pratul.in>
2010-09-16 16:51:35 +0530

    Updated PracticesList activity. Added PracticesList row layout.

--- gen/net/agropedia/pop/R.java
+++ gen/net/agropedia/pop/R.java
@@ -14,12 +14,14 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int mnu_about=0x7f060001;
+        public static final int mnu_about=0x7f060002;
         public static final int tv_homescreen_row=0x7f060000;
+        public static final int tv_practiceslist_row=0x7f060001;
     }
     public static final class layout {
         public static final int lv_home_row=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int lv_practiceslist_row=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class menu {
         public static final int about=0x7f050000;
--- src/net/agropedia/pop/PracticesList.java
+++ src/net/agropedia/pop/PracticesList.java
@@ -4,16 +4,73 @@
 
 package net.agropedia.pop;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class PracticesList extends ListActivity {
+
+	private PracticesListAdapter mAdapter;
+	private static Typeface tf;
+	private DBManager dbm;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		ListView lv = getListView();
+		mAdapter = new PracticesListAdapter();
+		tf = Typeface.createFromAsset(getAssets(), "fonts/Lohit-Hindi.ttf");
 
+		dbm = new DBManager(this);
+		dbm.open();
+
+		lv.setAdapter(mAdapter);
 	}
 
+
+	public class PracticesListAdapter extends BaseAdapter {
+    	private final ArrayList<String> mItemsList;
+    	private final LayoutInflater mInflater;
+    	TextView tvr;
+
+    	public PracticesListAdapter() {
+    		mItemsList = new ArrayList<String>(Arrays.asList("हरे चारे हेतु हाइब्रिड नेपियर की खेती", "बाजरा का हरा चारा", "ग्वार का हरा चारा", "ज्वार का हरा चारा", "मक्का का हरा चारा", "लोबिया का हरा चारा", "धान की उन्नतशील खेती", "Sugarcane Package of Practices", "Package of practices for Soyabean", "Package of practices for Maize"));
+    		mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    	}
+
+		public int getCount() {
+			return mItemsList.size();
+		}
+
+		public Object getItem(int position) {
+			return mItemsList.get(position);
+		}
+
+		public long getItemId(int position) {
+			return position;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+				convertView = mInflater.inflate(R.layout.lv_practiceslist_row, null);
+			}
+
+			tvr = (TextView) convertView.findViewById(R.id.tv_practiceslist_row);
+			tvr.setText(mItemsList.get(position));
+			tvr.setTypeface(tf);
+
+			return convertView;
+		}
+	}
 }
